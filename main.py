7c2913a8c82898a9141c10153d1aa186141e93ee
ayoub <ayoub@checkin.com>
2025-09-17 21:33:20 +0100

    feat: Ch3 functions calling, L2 function declaration

--- main.py
+++ main.py
@@ -17,22 +17,57 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 user_prompt = sys.argv[1]
 verbose = get_arg(2, '')
+system_propmt="""
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 messages = [
     types.Content(role='user', parts=[types.Part(text=user_prompt)]),
 ]
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            )
+        }
+    )
+)
+
+available_functions=types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 def main():
 
-    print("prompt: ", user_prompt)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_propmt),
     )
     if (verbose == '--verbose'):
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(response.function_calls) > 0: 
+        output = [f"Calling function: {call.name}({call.args})" for call in response.function_calls]
+
+        print("\n".join(output))
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
